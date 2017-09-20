0986cdc6617384347c520a4eed52a4785a1ef720
foreezh <522292344@qq.com>
2017-09-20 23:23:14 +0800

    项目模块化重构

--- public/js/common.js
+++ public/js/common.js
@@ -1,22 +1,26 @@
+define(['jquery'],function($){
+	//NProgress.start();
 
-	NProgress.start();
-
-	NProgress.done();
-
+	//NProgress.done();
+	//控制左侧菜单的折叠和展开
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
-
 	//实现退出功能
-$('#logoutBtn').click(function(){
-	$.ajax({
-		type:'post',
-		url:'/api/logout',
-		dataType:"json",
-		success:function(data){
-			if(data.code==200){
-				location.href='/main/login'
+	$('#logoutBtn').click(function(){
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:"json",
+			success:function(data){
+				if(data.code==200){
+					location.href='/main/login'
+				}
 			}
-		}
+		})
 	})
 })
+
+
+
+
---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+define(['jquery'],function($){
+	//实现登录功能
+	$('#login').click(function(){
+		$.ajax({
+	    	type:'post',
+	    	url:'/api/login',
+	    	data:$('#loginForm').serialize(),//获取表单的所有数据
+	    	dataType:'json',
+	    	success:function(data){
+	        	if(data.code==200){
+	            	//登陆到主页面
+	            	location.href='/main/index'
+	        	}else{
+	            	alert('用户名或者密码错误');
+	        	}
+	        
+	    	}
+		})
+		return false;
+	});
+})
